be424f29918e4e55fa39f0262229ca489404f777
Davide Casotto <davidecasotto31@gmail.com>
2021-09-10 17:35:26 +0200

    Main Activity: Button function 1

--- app/src/main/java/com/example/spaceinvader/MainActivity.kt
+++ app/src/main/java/com/example/spaceinvader/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.spaceinvader
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity() : AppCompatActivity(), View.OnClickListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View){
+        when(v.id){
+            R.id.bt_newGame -> toNewGame()
+            R.id.bt_scores -> toScore()
+            R.id.bt_exit -> toExit()
+            else -> return
+        }
+    }
+
+    private fun toNewGame(){
+        val intent = Intent(this, GameActivity::class.java).apply{}
+        startActivity(intent)
+    }
+
+    private fun toScore(){
+        val intent = Intent(this, ScoresActivity::class.java).apply {}
+        startActivity(intent)
+    }
+
+    private fun toExit(){
+        onDestroy()
     }
 }
