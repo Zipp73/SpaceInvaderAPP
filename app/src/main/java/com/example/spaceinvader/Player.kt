3e1a60a63542fe25072513a4b40aef9236889885
Luigi <ilgi97gi@gmail.com>
2021-09-11 18:04:01 +0200

    adjust home activity + developing firebase get data

--- app/src/main/java/com/example/spaceinvader/Player.kt
+++ app/src/main/java/com/example/spaceinvader/Player.kt
@@ -18,20 +18,15 @@ fun addPlayerToList(player: Player) {
     players.add(player)
 }
 
-//data class Player(val nickname: String = "", val score: Int = 0, var uuid: String = "")
-/*fun loadDatabase(firebaseData: DatabaseReference) {//TODO cambiare sturttura dati
-    val players: List<Player> = mutableListOf(
-        Player("Walu", 9385743),
-        Player("Bows", 6843854),
-        Player("Cayu", 2894326)
-    )
+/*
+fun loadDatabase(firebaseData: DatabaseReference) {
     players.forEach {
         val key = firebaseData.child("players").push().key
-        if (key != null) {
-            it.uuid = key
-        }
-        if (key != null) {
-            firebaseData.child("players").child(key).setValue(it)
-        }
+        it.uuid = key
+        firebaseData.child("players").child(key).setValue(it)
     }
-}*/
+}
+ */
+
+
+
--- app/src/main/java/com/example/spaceinvader/ScoresActivity.kt
+++ app/src/main/java/com/example/spaceinvader/ScoresActivity.kt
@@ -1,36 +1,56 @@
 package com.example.spaceinvader
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-/**/import com.google.firebase.database.*
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
-
-//val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-//val mRootRef: DatabaseReference = database.getReference()//players
-
+val database = Firebase.database    //write
+val mRootRef = database.reference    //write .getReference("players")
 
 class ScoresActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scores)
 
-        val database = Firebase.database    //write
-        val mRootRef = database.reference    //write .getReference("players")
+            //WRITE
+        //val player1 = Player("Luck", 3459876)//todo
+        //addPlayerToList(player1)//TODO for test
+        //loadDatabase(mRootRef)
 
-        val player1 = Player("Walu", 8543667)
-        addPlayerToList(player1)//TODO for test
-
-        loadDatabase(mRootRef)
+        getData(mRootRef)
     }
 
     override fun onClick(v: View){
         when(v.id){
             R.id.bt_return -> finish()
-
+            R.id.bt_refresh -> getData(mRootRef)
         }
+    }
+
+    fun getData(firebaseData: DatabaseReference) {//todo adjust
+            //READ
+        mRootRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<Player>()
+                //Log.d(TAG, "Value is: $value")
+                if (value != null) Toast.makeText(this@ScoresActivity, "$value", Toast.LENGTH_LONG).show()
+            }
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+        })
     }
 
 }
